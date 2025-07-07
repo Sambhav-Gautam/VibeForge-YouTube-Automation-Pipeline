3602e1718551f9dbcf9a3c3100f88a29009ec7a7
Sambhav-Gautam <sambhav22435@iiitd.ac.in>
2025-07-07 22:27:22 +0530

    AI Models : Channel Added

--- file.py
+++ file.py
@@ -4,10 +4,10 @@ import pickle
 SCOPES = ["https://www.googleapis.com/auth/youtube.upload"]
 
 flow = InstalledAppFlow.from_client_secrets_file(
-    "Secrets/client_secret_Illusions.json",
+    "Secrets/client_secret_ai_models.json",
     scopes=SCOPES
 )
 creds = flow.run_local_server(port=0)
 
-with open("token_illusions.pickle", "wb") as token_file:
+with open("token_ai_models.pickle", "wb") as token_file:
     pickle.dump(creds, token_file)
